8cf6d59c2ab0e8e8628221632f1912503afff762
Alejandro Peña Carbonell <alex@render.es>
2023-12-07 21:24:53 +0100

    changed the auction number from the principal coin, Now every coins show his number by itself

--- tpl/type/js/type_row_fields.js
+++ tpl/type/js/type_row_fields.js
@@ -1583,14 +1583,14 @@ export const type_row_fields = {
 
 			if (data.ref_auction_group) {
 				for (let i = 0; i < data.ref_auction_group.length; i++) {
-					data.ref_auction_group[i].lot = data.number
+					// data.ref_auction_group[i].lot = data.number
 					draw_auction(data.ref_auction_group[i], wrapper, "identify_coin", '')
 				}
 			}
 
 			if (data.ref_related_coin_auction_group) {
 				for (let i = 0; i < data.ref_related_coin_auction_group.length; i++) {
-					data.ref_related_coin_auction_group[i].lot = data.number
+					// data.ref_related_coin_auction_group[i].lot = data.number
 					draw_auction(data.ref_related_coin_auction_group[i], wrapper, "identify_coin", '= ')
 				}
 			}
