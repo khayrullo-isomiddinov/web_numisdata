5c81d885ee35534e23df902e78aabe6221ba7c39
Paco <paco@render.es>
2024-07-10 12:20:05 +0200

    style: clean up code

--- tpl/page/js/footer.js
+++ tpl/page/js/footer.js
@@ -195,7 +195,9 @@ var footer =  {
 					const child_tipo	= children[i]
 					const child			= self.footer_data.find(el => el.term_id===child_tipo)
 					if (!child) {
-						console.log("Ignore not found children:", row, child_tipo, children);
+						if(SHOW_DEBUG===true) {
+							console.log("Ignore not found children:", row, child_tipo, children);
+						}
 						continue;
 					}
 
