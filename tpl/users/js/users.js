ce9a08bc47b883201dfaa7489300e061e5b6b3f3
Paco <paco@render.es>
2025-04-10 13:29:09 +0200

    fix: fix non defined user row issue that crash the users area. Now, user activity `what` is checked before proceed.

--- tpl/users/js/users.js
+++ tpl/users/js/users.js
@@ -169,7 +169,14 @@ var users =  {
 			const parsed_data_length = parsed_data.length
 			for (let i = 0; i < parsed_data_length; i++) {
 
-				const row	= parsed_data[i]
+				const row = parsed_data[i]
+
+				if (!row || !row.activity || !row.activity.what) {
+					if(SHOW_DEBUG===true) {
+						console.log('Ignored empty activity what row:', row );
+					}
+					continue;
+				}
 
 				const save_data = row.activity.what.find(el => el.key==='dd700')
 				if (save_data && row.full_name) {
